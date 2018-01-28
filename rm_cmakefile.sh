57e7405a14c091bc2747399c3084fd34d4fe700c
lester.hu <lester.hu@primax.com.tw>
2018-01-28 20:22:09 +0000

    update cmake structure

--- rm_cmakefile.sh
+++ rm_cmakefile.sh
@@ -7,7 +7,10 @@ if [ -z "$1" ]; then
 	exit 1
 fi 
 
-$RM $1/bin/*
+$RM $1/$(find . -name CMakeFiles)
+$RM $1/$(find . -name cmake_install.cmake)
+$RM $1/$(find . -name Makefile)
+
 $RM $1/CMakeCache.txt
 $RM $1/CMakeFiles
 $RM $1/cmake_install.cmake
