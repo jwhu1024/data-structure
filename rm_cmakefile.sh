9b49c59b98ce6b1cf1e5357b0fb2d4cf519b2364
lester.hu <lester.hu@primax.com.tw>
2018-01-30 14:32:50 +0000

    update rm_cmakefile.sh

--- rm_cmakefile.sh
+++ rm_cmakefile.sh
@@ -2,6 +2,8 @@
 
 RM="rm -rvf"
 PWD=$(pwd)
+PRJ_SINGLY="singly"
+PRJ_CIRCUL="circular"
 
 $RM $PWD/$(find . -name CMakeFiles)
 $RM $PWD/$(find . -name cmake_install.cmake)
@@ -11,3 +13,7 @@ $RM $PWD/CMakeCache.txt
 $RM $PWD/CMakeFiles
 $RM $PWD/cmake_install.cmake
 $RM $PWD/Makefile
+
+$RM $PWD/linked_list/$PRJ_SINGLY/bin/
+$RM $PWD/linked_list/$PRJ_CIRCUL/bin/
+
