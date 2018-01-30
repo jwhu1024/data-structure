d2407abd54e3509a6c740c71561c543430e96358
lester.hu <lester.hu@primax.com.tw>
2018-01-30 14:48:34 +0000

    initial stack project

--- stack/inc/stack.h
+++ stack/inc/stack.h
@@ -1,58 +1,7 @@
-#ifndef __CIRCULAR_LINKED_LIST__
-#define __CIRCULAR_LINKED_LIST__
+#ifndef __STACK_STRUCT__
+#define __STACK_STRUCT__
 
-#include <stdbool.h>
+#define STACK_MINIMAL_NODE	1
+#define STACK_MAXIMAL_NODE	100
 
-#define CLIST_MINIMAL_NODE	1
-#define CLIST_MAXIMAL_NODE	100
-
-/* caller handle definition */
-typedef struct _CLIST_NODE_H_
-{
-	/* number of the nodes in list */
-	unsigned short count;
-	
-	/* pointer to the first node in list */
-	struct clist_node *head;
-
-	/* just pointer to last node we operated */
-	struct clist_node *cur;
-} CLIST_NODE_H, *PCLIST_NODE_H;
-
-/* custom structure */
-typedef struct _CLIST_CUSTOM_ {
-	int age;
-	char name[16];
-} CLIST_CUSTOM_DATA, *PCLIST_CUSTOM_DATA;
-
-/* list node definition */
-struct clist_node
-{
-	CLIST_CUSTOM_DATA custom_data;
-	struct clist_node *next;
-};
-
-/* create and destroy */
-bool clist_destroy (CLIST_NODE_H *h);
-bool clist_init (CLIST_NODE_H *h);
-
-/* clist insert function */
-bool clist_insert_node_front (CLIST_NODE_H *h, PCLIST_CUSTOM_DATA data);
-bool clist_insert_node_tail (CLIST_NODE_H *h, PCLIST_CUSTOM_DATA data);
-bool clist_insert_node_index (CLIST_NODE_H *h, PCLIST_CUSTOM_DATA data, int idx);
-
-/* clist remove function */
-bool clist_remove_node_front (CLIST_NODE_H *h);
-bool clist_remove_node_tail (CLIST_NODE_H *h);
-bool clist_remove_node_index (CLIST_NODE_H *h, int idx);
-
-/* search in clist */
-bool clist_search_node (CLIST_NODE_H *h, char *name);
-
-/* get clist length */
-int clist_length (CLIST_NODE_H *h);
-
-/* clist debug method */
-void clist_print (CLIST_NODE_H *h);
-
-#endif	/* __CIRCULAR_LINKED_LIST__ */
+#endif	/* __STACK_STRUCT__ */
--- stack/src/main.c
+++ stack/src/main.c
@@ -3,148 +3,15 @@
 #include <string.h>
 #include <time.h>
 #include "debug.h"
-#include "clist.h"
+#include "stack.h"
 
 #define MAIN_DBG(message, ...)	\
  		MSG("%s" message "%s",   LIGHT_GREEN, ##__VA_ARGS__, RESET)
 
-void test_case_insert_front_1(CLIST_NODE_H *h)
-{
-	CLIST_CUSTOM_DATA my_data;
-	PCLIST_CUSTOM_DATA pdata = &my_data;
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Aaron");
-	clist_insert_node_front (h, pdata);
-	clist_print(h);
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Eagle");
-	clist_insert_node_front (h, pdata);
-	clist_print(h);
-}
-
-void test_case_insert_tail_1(CLIST_NODE_H *h)
-{
-	CLIST_CUSTOM_DATA my_data;
-	PCLIST_CUSTOM_DATA pdata = &my_data;
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Alastair");
-	clist_insert_node_tail (h, pdata);
-	clist_print(h);
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Badger");
-	clist_insert_node_tail (h, pdata);
-	clist_print(h);
-}
-
-void test_case_insert_index_1(CLIST_NODE_H *h)
-{
-	CLIST_CUSTOM_DATA my_data;
-	PCLIST_CUSTOM_DATA pdata = &my_data;
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Carver");
-	clist_insert_node_index (h, pdata, 1);
-	clist_print(h);
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Casey");
-	clist_insert_node_index (h, pdata, clist_length(h));
-	clist_print(h);
-}
-
-void test_case_remove_front_1(CLIST_NODE_H *h)
-{
-	CLIST_CUSTOM_DATA my_data;
-	PCLIST_CUSTOM_DATA pdata = &my_data;
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Rocky");
-	clist_insert_node_front (h, pdata);
-	clist_print(h);
-
-	clist_remove_node_front(h);
-	clist_print(h);
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Farley");
-	clist_insert_node_front (h, pdata);
-	clist_print(h);
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Rocky");
-	clist_insert_node_front (h, pdata);
-	clist_print(h);
-
-	clist_remove_node_front(h);
-	clist_print(h);
-}
-
-void test_case_remove_tail_1(CLIST_NODE_H *h)
-{
-	CLIST_CUSTOM_DATA my_data;
-	PCLIST_CUSTOM_DATA pdata = &my_data;
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Donald");
-	clist_insert_node_front (h, pdata);
-	clist_print(h);
-
-	clist_remove_node_tail(h);
-	clist_print(h);
-	
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Derek");
-	clist_insert_node_tail (h, pdata);
-	clist_print(h);
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Egerton");
-	clist_insert_node_tail (h, pdata);
-	clist_print(h);
-	
-	clist_remove_node_tail(h);
-	clist_print(h);
-}
-
-void test_case_remove_index_1(CLIST_NODE_H *h)
-{
-	CLIST_CUSTOM_DATA my_data;
-	PCLIST_CUSTOM_DATA pdata = &my_data;
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Freeman");
-	clist_insert_node_tail (h, pdata);
-	clist_print(h);
-
-	clist_remove_node_index(h, 1);
-	clist_print(h);
-	
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Kelvin");
-	clist_insert_node_tail (h, pdata);
-	clist_print(h);
-
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Leonard");
-	clist_insert_node_tail (h, pdata);
-	clist_print(h);
-	
-	pdata->age = (rand() % 100) + 1;
-	strcpy(pdata->name, "Lucas");
-	clist_insert_node_tail (h, pdata);
-	clist_print(h);
-	
-	clist_remove_node_index(h, clist_length(h));
-	clist_print(h);
-}
-
 int main (int argc, char* argv[])
 {
 	MAIN_DBG("BEGIN\n");
+#if 0
 	CLIST_NODE_H my_h;
 
 	srand(time(NULL));
@@ -190,7 +57,7 @@ int main (int argc, char* argv[])
 		MAIN_DBG("memory destroy has some problems!\n");
 		return -1;
 	}
-
+#endif
 	MAIN_DBG("END\n");
 	return 0;
 }
